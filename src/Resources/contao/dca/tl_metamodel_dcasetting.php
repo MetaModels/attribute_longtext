a191de9674dc83b6dc882cd984c3bfa01f1d15e2
zonky <info@e-spin.de>
2023-07-17 09:57:29 +0200

    Add feature to chose own widget template

--- src/Resources/contao/dca/tl_metamodel_dcasetting.php
+++ src/Resources/contao/dca/tl_metamodel_dcasetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_longtext.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2023 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,14 +15,19 @@
  * @author     Christopher Boelter <c.boelter@cogizz.de>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2023 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_longtext/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
+use Contao\System;
+use MetaModels\ContaoFrontendEditingBundle\MetaModelsContaoFrontendEditingBundle;
+
 $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id']['longtext'] = [
     'presentation' => [
         'tl_class',
+        'be_template',
         'rte',
         'rows',
         'cols',
@@ -40,3 +45,13 @@ $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id'
         'searchable',
     ]
 ];
+
+// Load configuration for the frontend editing.
+if (\in_array(
+    MetaModelsContaoFrontendEditingBundle::class,
+    System::getContainer()->getParameter('kernel.bundles'),
+    true
+)) {
+    $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id']['longtext']['presentation'][] =
+        'fe_template';
+}
